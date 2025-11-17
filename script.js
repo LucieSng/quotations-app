5f0dfb559cefe958acd6641c77be50b8c9756a47
Lucie Seng <lucie.sng@gmail.com>
2025-11-17 11:19:19 +0100

    fix : add user quote

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
+// Etape 4
 // Je crée les variables qui permettent de récupérer les input de l'utilisateur
 const inputQuote = document.getElementById("citation");
 const inputAuthor = document.getElementById("auteur");
 const submit = document.getElementById("form");
 const resultSubmit = document.getElementById("quote-list");
 
-// Je veux qu'au clic, la citation s'affiche en HTML
 submit.addEventListener("submit", function (e) {
   // Pour que la page ne se recharge pas à chaque citation ajoutée
   e.preventDefault();
-  // Je veux ajouter la citation et l'auteur saisis par l'utilisateur
-  resultSubmit.innerHTML += `<div class="quote">
-        <p class="text">${inputQuote.value}</p>
-        <p class="author">${inputAuthor.value}</p>
-      </div>
-`;
+  const text = inputQuote.value;
+  const author = inputAuthor.value;
+  // Afficher dans la console
+  console.log(text, author);
 });
+// Je veux afficher les citations saisies directement sur la page
+// function addQuote(inputQuote, inputAuthor) {}
+
+// Je veux qu'au clic, la citation s'affiche en HTML
+// submit.addEventListener("submit", function (e) {
+//   // Pour que la page ne se recharge pas à chaque citation ajoutée
+//   e.preventDefault();
+//   // Je veux ajouter la citation et l'auteur saisis par l'utilisateur
+//   resultSubmit.innerHTML += `<div class="quote">
+//         <p class="text">${inputQuote.value}</p>
+//         <p class="author">${inputAuthor.value}</p>
+//       </div>
+// `;
+// });
